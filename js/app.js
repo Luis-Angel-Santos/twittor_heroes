78b2206b9c5f9c3ad0ce47b4ffb72c8629e6f545
Luis Angel Santos <santosangel748@gmail.com>
2022-10-11 14:40:20 -0500

    Cache con Network Fallback

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-if(navigator.serviceWorker){
-    navigator.serviceWorker.register('/sw.js');
-}
+if (navigator.serviceWorker){
+    navigator.serviceWorker.register('/sw.js')
+};
 
 
 // Referencias de jQuery
--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+importScripts('js/sw-utils.js');
+
 const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
@@ -13,6 +15,7 @@ const APP_SHELL = [
     'img/avatars/thor.jpg',
     'img/avatars/wolverine.jpg',
     'js/app.js',
+    'js/sw-utils.js'
 ];
 
 const APP_SHELL_INMUTABLE = [
@@ -24,12 +27,11 @@ const APP_SHELL_INMUTABLE = [
 ];
 
 self.addEventListener('install', e => {
-    const cacheStatic = caches.open(STATIC_CACHE).then(cache => 
-        cache.addAll(APP_SHELL));
-    const cacheInmutable = caches.open(INMUTABLE_CACHE).then(cache => 
-        cache.addAll(APP_SHELL_INMUTABLE));
-
-    e.waitUntil(Promise.all([cacheStatic, cacheInmutable]));
+    const cacheStatic = caches.open( STATIC_CACHE ).then(cache => 
+        cache.addAll( APP_SHELL ));
+    const cacheInmutable = caches.open( INMUTABLE_CACHE ).then(cache => 
+        cache.addAll( APP_SHELL_INMUTABLE ));
+    e.waitUntil( Promise.all([ cacheStatic, cacheInmutable ])  );
 });
 
 self.addEventListener('activate', e => {
@@ -41,4 +43,17 @@ self.addEventListener('activate', e => {
         });
     });
     e.waitUntil( respuesta );
+});
+
+self.addEventListener('fetch', e => {
+    const respuesta = caches.match(e.request).then(resp => {
+        if(resp){
+            return resp;
+        }else{
+            return fetch(e.request).then(newResp => {
+                return actualizarCacheDinamico(DYNAMIC_CACHE, e.request, newResp);
+            })
+        }
+    });
+    e.respondWith(respuesta);
 });
