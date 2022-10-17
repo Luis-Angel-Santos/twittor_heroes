5a9f5eea992d191d6b58484df1ef224e3521bd60
Luis Angel Santos <santosangel748@gmail.com>
2022-10-17 13:21:55 -0500

    Utilizando github pages y mejorando la apariencia en ios

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ importScripts('js/sw-utils.js');
 
 
 const STATIC_CACHE    = 'static-v4';
-const DYNAMIC_CACHE   = 'dynamic-v2';
+const DYNAMIC_CACHE   = 'dynamic-v3';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 
@@ -40,25 +40,25 @@ self.addEventListener('install', e => {
 
 
 self.addEventListener('activate', e => {
-
+ 
     const respuesta = caches.keys().then( keys => {
-
+ 
         keys.forEach( key => {
-
+ 
             if (  key !== STATIC_CACHE && key.includes('static') ) {
                 return caches.delete(key);
             }
-
+ 
             if (  key !== DYNAMIC_CACHE && key.includes('dynamic') ) {
                 return caches.delete(key);
             }
-
+ 
         });
-
+ 
     });
-
+ 
     e.waitUntil( respuesta );
-
+ 
 });
 
 
